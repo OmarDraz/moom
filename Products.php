7ad6b72044882d17640cd78b9a8aa35b4e5c24c9
MOHAMEDHESMAM8 <59764426+MOHAMEDHESMAM8@users.noreply.github.com>
2020-12-30 17:37:12 +0200

    حطيت الhtml اقبل

--- Products.php
+++ Products.php
@@ -10,11 +10,389 @@
         <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="products">
-<?php
+        <?php
     echo $_SESSION['Username'];
                 ?>
+        <input  type="checkbox" class="checkbox" id="checkid">
+        <div class="poup-background">&nbsp;</div>
+        <div class="nav-container">
+            <ul class="popup-menu">
+                <li><a href="">E-Commerce</a></li>
+                <li><a href="">Freelancing</a></li>
+                <li><a href="">Education</a></li>
+                <li><a href="">Small Shops</a></li>
+                <li><a href="">Companies Systems</a></li>
+                <li><a href="">CRMs</a></li>
+                <li><a href="">Chatting Apps</a></li>
+                <li><a href="">Portofolios</a></li>
+            </ul>
         </div>
+        <nav>
+            <div class="top">
+                
+                <label for="checkid"  class="menu"><i class="fas fa-bars"></i></label>
+                <img src="Image/logo.png" alt="Logo|M&#8734M ">
+                <form class="input-search">
+                    <input type="search"  class="search" placeholder="Search Here ...">
+                    <i class="fa fa-search"></i>
+                </form>
+                <ul class="nav-icons">
+                    <li class="search-icon"> <a ><i class="fa fa-search"></i></a></li>
+                    <li class="icon-none"><a href=""><i class="fa fa-heart"></i></a></li>
+                   <li> <a href=""><i class="fa fa-shopping-cart"></i></a></li>
+                   <li class="icon-none"> <a href=""><i class="fa fa-user"></i></a></li>
+                   <li class="sign-out">Sign Out</li>
+                </ul>
+            </div>
+            <div class="bottom">
+                <ul class="category-menu">
+                    <li><a href="">E-Commerce</a></li>
+                    <li><a href="">Freelancing</a></li>
+                    <li><a href="">Education</a></li>
+                    <li><a href="">Small Shops</a></li>
+                    <li><a href="">Companies Systems</a></li>
+                    <li><a href="">CRMs</a></li>
+                    <li><a href="">Chatting Apps</a></li>
+                    <li><a href="">Portofolios</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="mainBody">
+            <a href="#popup-filter" class="filter-icon">
+                <i class="fas fa-filter"></i>
+            </a>
+            <div class="left-side" id="popup-filter">
+                <div class="filter">
+                    <a href=" " class="closebutton" >
+                        <i class="fas fa-times"></i>
+                    </a>
+                    <div class="filter-container">
+                        <h4>Brand</h4> 
+                        <span class="button" ><i  class="fa fa-plus"></i></span>
+                        <span class="button"><i  class="fa fa-minus"></i></span>
+                        <div class="search-options">
+                            <input type="search" id="search-input"placeholder="Seach" >
+                            <ul >
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="filter-container">
+                        <h4>Brand</h4> 
+                        <span class="button" ><i  class="fa fa-plus"></i></span>
+                        <span class="button"><i  class="fa fa-minus"></i></span>
+                        <div class="search-options">
+                            <input type="search" id="search-input"placeholder="Seach" >
+                            <ul >
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="filter-container">
+                        <h4>Brand</h4> 
+                        <span class="button" ><i  class="fa fa-plus"></i></span>
+                        <span class="button"><i  class="fa fa-minus"></i></span>
+                        <div class="search-options">
+                            <input type="search" id="search-input"placeholder="Seach" >
+                            <ul >
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="filter-container">
+                        <h4>Brand</h4> 
+                        <span class="button" ><i  class="fa fa-plus"></i></span>
+                        <span class="button"><i  class="fa fa-minus"></i></span>
+                        <div class="search-options">
+                            <input type="search" id="search-input"placeholder="Seach" >
+                            <ul >
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                                <LI>MOHFGXGJX</LI>
+                                <li>jhbjhjhvjh</li>
+                                <li>jgvhgctycyfh</li>
+                                <li>hcdsazsxdfcgh</li>
+                            </ul>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="right-side">
+                <div class="view">
+                    <span>View :</span>
+                    <span class="active-view"> <i  class="fas fa-th-large more"></i> </span>
+                    <span><i  class="fas fa-th large"></i></span>
+                </div>
+                <div class="product-container">
+                    <div class="product-card">
+                        <img src="Image/image 1.jpg" >
+                        <div class="card-content">
+                            <h3> Product Name </h3>
+                            <div class="starts">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="price">
+                                <p>200$</p>
+                            </div>
+                         
+                            <div class="card-description ">
+                                <ul>
+                                    <li>Mnkdjs</li>
+                                    <li>Mskjndsjnd</li>
+                                    <li>M</li>
+                                    <li>M</li>
+
+                                </ul>
+                            </div>
+                            <div class="card-btn">
+                                <button class="chat-btn">
+                                    <i class="fas fa-comments"></i>Chat
+                                </button>
+                                <button class="create-btn">
+                                    <i class="fa fa-plus"></i> Create
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-card">
+                        <img src="Image/image 1.jpg" >
+                        <div class="card-content">
+                            <h3> Product Name </h3>
+                            <div class="starts">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="price">
+                                <p>200$</p>
+                            </div>
+                         
+                            <div class="card-description ">
+                                <ul>
+                                    <li>Mnkdjs</li>
+                                    <li>Mskjndsjnd</li>
+                                    <li>M</li>
+                                    <li>M</li>
+
+                                </ul>
+                            </div>
+                            <div class="card-btn">
+                                <button class="chat-btn">
+                                    <i class="fas fa-comments"></i>Chat
+                                </button>
+                                <button class="create-btn">
+                                    <i class="fa fa-plus"></i> Create
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-card">
+                        <img src="Image/image 1.jpg" >
+                        <div class="card-content">
+                            <h3> Product Name </h3>
+                            <div class="starts">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="price">
+                                <p>200$</p>
+                            </div>
+                         
+                            <div class="card-description ">
+                                <ul>
+                                    <li>Mnkdjs</li>
+                                    <li>Mskjndsjnd</li>
+                                    <li>M</li>
+                                    <li>M</li>
+
+                                </ul>
+                            </div>
+                            <div class="card-btn">
+                                <button class="chat-btn">
+                                    <i class="fas fa-comments"></i>Chat
+                                </button>
+                                <button class="create-btn">
+                                    <i class="fa fa-plus"></i> Create
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-card">
+                        <img src="Image/image 1.jpg" >
+                        <div class="card-content">
+                            <h3> Product Name </h3>
+                            <div class="starts">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="price">
+                                <p>200$</p>
+                            </div>
+                         
+                            <div class="card-description ">
+                                <ul>
+                                    <li>Mnkdjs</li>
+                                    <li>Mskjndsjnd</li>
+                                    <li>M</li>
+                                    <li>M</li>
+
+                                </ul>
+                            </div>
+                            <div class="card-btn">
+                                <button class="chat-btn">
+                                    <i class="fas fa-comments"></i>Chat
+                                </button>
+                                <button class="create-btn">
+                                    <i class="fa fa-plus"></i> Create
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-card">
+                        <img src="Image/image 1.jpg" >
+                        <div class="card-content">
+                            <h3> Product Name </h3>
+                            <div class="starts">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="price">
+                                <p>200$</p>
+                            </div>
+                         
+                            <div class="card-description ">
+                                <ul>
+                                    <li>Mnkdjs</li>
+                                    <li>Mskjndsjnd</li>
+                                    <li>M</li>
+                                    <li>M</li>
+
+                                </ul>
+                            </div>
+                            <div class="card-btn">
+                                <button class="chat-btn">
+                                    <i class="fas fa-comments"></i>Chat
+                                </button>
+                                <button class="create-btn">
+                                    <i class="fa fa-plus"></i> Create
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-card">
+                        <img src="Image/image 1.jpg" >
+                        <div class="card-content">
+                            <h3> Product Name </h3>
+                            <div class="starts">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                            </div>
+                            <div class="price">
+                                <p>200$</p>
+                            </div>
+                         
+                            <div class="card-description ">
+                                <ul>
+                                    <li>Mnkdjs</li>
+                                    <li>Mskjndsjnd</li>
+                                    <li>M</li>
+                                    <li>M</li>
+
+                                </ul>
+                            </div>
+                            <div class="card-btn">
+                                <button class="chat-btn">
+                                    <i class="fas fa-comments"></i>Chat
+                                </button>
+                                <button class="create-btn">
+                                    <i class="fa fa-plus"></i> Create
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="js/JQuery3.3.1.js"></script>
+        <script type="text/javascript" src="js/search.js"></script>
+       <script type="text/javascript" src="js/search-filte.js"></script>  
+       <script type="text/javascript" src="js/search-options.js"></script>
+       <script type="text/javascript" src="js/products-view.js"></script>   
+
     </body>
 </html>
 
